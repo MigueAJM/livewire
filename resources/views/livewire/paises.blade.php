87859d5e7dda6452a26666a8f5d99730b96d9806
migue <miguejm@outlook.es>
2021-03-01 21:30:15 -0600

    Eventos y componentes

--- resources/views/livewire/paises.blade.php
+++ resources/views/livewire/paises.blade.php
@@ -1,10 +1,13 @@
 <div>
     <h1>Lista de Paises</h1>
-
-    <input type="text" placeholder="Ingresar un nuevo País" wire:model="pais"/>
-
+    <form action="" wire:submit.prevent="save">
+        <input type="text" placeholder="Ingresar un nuevo País" wire:model="pais"/>
+        <button >Guardar</button>
+    </form>  
+    <!--
+    <input type="text" placeholder="Ingresar un nuevo País" wire:model="pais" wire:keydown.enter="save"/>
     <button wire:click="save">Guardar</button>
-
+    -->  
     <ul>
         @foreach ($paises as $pais)
             <li>{{$pais}}</li>
