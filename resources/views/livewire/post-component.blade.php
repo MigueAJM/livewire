4f206688dcae9b5e857d93e680b589dac805a87e
migue <miguejm@outlook.es>
2021-03-09 23:23:30 -0600

    Estilos

--- resources/views/livewire/post-component.blade.php
+++ resources/views/livewire/post-component.blade.php
@@ -1,22 +1,41 @@
 <div class="container mx-auto">
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Body</th>
+
+    <div class="bg-white rounded-lg shadow overflow-hidden max-w-4xl mx-auto p-4 mb-6">
+        <div class="mb-3">
+            <label for="name" class="form-label mb-2">Nombre</label>
+            <input id="name" name="body" placeholder="Ingrese un nombre"
+            class="form-control">
+        </div>
+
+        <div class="mb-3">
+            <label for="body" class="form-label mb-2">Descripción</label>
+            <textarea  name="body" id="body" rows="4"
+            placeholder="Ingrese la descripción del post" class="form-control"></textarea>
+        </div>
+
+        <div>
+            <button class="btn-primary">Crear post</button>
+        </div>
+    </div>
+
+    <table class="bg-white rounded-lg shadow overflow-hidden max-w-4xl mx-auto">
+        <thead class="bg-gray-50 border-b border-gray-200">
+            <tr class="text-xs font-medium text-gray-500 uppercase text-left tracking-wider">
+                <th class="px-6 py-3">ID</th>
+                <th class="px-6 py-3">Nombre</th>
+                <th class="px-6 py-3">Body</th>
                 <th></th>
             </tr>
         </thead>
-        <tbody>
+        <tbody class="divide-y divide-gray-200">
             @foreach ($posts as $post)
-                <tr>
-                    <td>{{$post->id}}</td>
-                    <td>{{$post->name}}</td>
-                    <td>{{$post->body}}</td>
-                    <td>
-                        <button>Editar</button>
-                        <button>Eliminar</button>
+                <tr class="text-sm text-gray-500">
+                    <td class="px-6 py-4">{{$post->id}}</td>
+                    <td class="px-6 py-4">{{$post->name}}</td>
+                    <td class="px-6 py-4">{{$post->body}}</td>
+                    <td class="px-6 py-4">
+                        <button class="btn-primary w-full">Editar</button>
+                        <button class="btn-secundary">Eliminar</button>
                     </td>
                 </tr>
             @endforeach
